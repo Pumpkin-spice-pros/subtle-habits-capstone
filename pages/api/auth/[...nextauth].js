39e5aaa2b038a8dc305817c805f0eab093051574
Aaron-Celia <card123er@gmail.com>
2023-03-29 13:10:20 -0400

    pages/api/auth/[...nextauth].js file changed, commented out the code that was replaced

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,70 @@
-import NextAuth from "next-auth"
-import { SupabaseAdapter } from "@next-auth/supabase-adapter"
+// import NextAuth from "next-auth"
+// import { SupabaseAdapter } from "@next-auth/supabase-adapter"
 
-// For more information on each option (and a full list of options) go to
-// https://authjs.dev/reference/configuration/auth-config
-export default NextAuth({
-  // https://authjs.dev/reference/providers/oauth-builtin
-  providers: [],
+// // For more information on each option (and a full list of options) go to
+// // https://authjs.dev/reference/configuration/auth-config
+// export default NextAuth({
+// 	// https://authjs.dev/reference/providers/oauth-builtin
+// 	providers: [
+// 		EmailProvider({
+// 			server: process.env.EMAIL_SERVER,
+// 			from: process.env.EMAIL_FROM
+// 			// maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+// 		})
+// 	],
+// 	adapter: SupabaseAdapter({
+// 		url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+// 		secret: process.env.SUPABASE_SERVICE_ROLE_KEY
+// 	})
+// 	// ...
+// });
+
+
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient('supabase-url', 'supabase-anon-key');
+
+const options = {
+  providers: [
+    Providers.Credentials({
+      name: 'Supabase',
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const { user, error } = await supabase.auth.signIn({
+          email: credentials.email,
+          password: credentials.password
+        });
+
+        if (error) {
+          throw new Error(error.message);
+        }
+
+        return user;
+      }
+    })
+  ],
   adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  }),
-  // ...
-})
+		url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+		secret: process.env.SUPABASE_SERVICE_ROLE_KEY
+	}),
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    session: async (session, user) => {
+      session.user.id = user.id;
+      return Promise.resolve(session);
+    }
+  },
+  secret: process.env.SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET
+  }
+};
+
+export default (req, res) => NextAuth(req, res, options);
